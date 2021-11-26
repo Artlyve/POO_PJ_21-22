fb51e098d9df04252c65ef89006a0c2c7f9639af
HizeOok <47179772+HizeOok@users.noreply.github.com>
2021-11-26 16:29:45 +0100

    Weapons

--- JEU/src/CosmicalRayBlaster.java
+++ JEU/src/CosmicalRayBlaster.java
@@ -1,2 +1,12 @@
 public class CosmicalRayBlaster extends Weapon {
+
+    public CosmicalRayBlaster(int d, Rarity r, IdentityItem id, boolean b, int durability){
+        super.setDamage( d );
+        super.setMyRarity( r );
+        super.setId( id );
+
+        super.setDurability( durability );
+        super.setUsable( b );
+    }
+
 }
--- JEU/src/SilverDagger.java
+++ JEU/src/SilverDagger.java
@@ -1,2 +1,12 @@
 public class SilverDagger extends Weapon {
+
+
+    public SilverDagger(int d, Rarity r, IdentityItem id, boolean b, int durability){
+        super.setDamage( d );
+        super.setMyRarity( r );
+        super.setId( id );
+
+        super.setDurability( durability );
+        super.setUsable( b );
+    }
 }
